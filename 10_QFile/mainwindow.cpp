0edc9ac041b35232946cb5f98ef9b502fc981c4c
troyeeeee673 <192567797+troyeeee673@users.noreply.github.com>
2026-07-09 18:41:08 +0800

    使用QFile打开关闭文件

---
+++ 10_QFile/mainwindow.cpp
@@ -0,0 +1,54 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QFileDialog>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this, "选择文件", "D:/ov_Watch/OV-Watch");
+    qDebug() <<"filename:"<<filename<< Qt::endl;
+    //设置要打开的文件
+    file.setFileName(filename);
+    //打开文件
+    if(!file.open(QIODevice::ReadWrite))
+    {
+        qDebug() <<"open file failed"<< Qt::endl;
+        return ;
+    }
+    ui->textEdit->setPlainText(file.readAll());
+
+    //关闭文件
+    file.close();
+
+
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    if(file.fileName().isEmpty())
+    {
+        qDebug()<<"no file opening"<<Qt::endl;
+        return;
+    }
+    file.open(QIODevice::ReadWrite);
+    file.write(ui->textEdit->toPlainText().toUtf8());
+    file.close();
+    ui->textEdit->clear();
+
+}
+
---
+++ 10_QFile/mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QFile file;
+};
+#endif // MAINWINDOW_H
